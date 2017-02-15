44d93f320c7661275d9098d323a62128312ba4e0
Rahul Roshan <razorroshan1911@gmail.com>
2017-02-16 01:50:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var content={
+var article={
     title:'Article One',
     heading:'Article one',
     content:`<p>
@@ -52,7 +52,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 app.get('/article-one',function (req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createtemplate(article));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
