f1339854180acb50cec430d3d6ff00e4b2d8f596
Rahul Roshan <razorroshan1911@gmail.com>
2017-03-05 15:17:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,11 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var c=0;
+app.get('/counter', function (req, res)
+{
+    c++;
+});
 
 app.get('/:articleName',function (req,res){
     var articleName=req.params.articleName;
